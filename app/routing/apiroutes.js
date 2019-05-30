606cc1c3d511ca441f26de1297fdbedadb5db876
Adam S <leavinit@gmail.com>
2019-05-30 00:29:14 -0500

    Added the survey questions on survey.html and began work on index.html

--- app/routing/apiroutes.js
+++ app/routing/apiroutes.js
@@ -1,12 +1,34 @@
 var express = require('express');
 var path = require("path");
+var app = module.exports = express();  
 
-module.exports = function(app){
 
-    app.get("/api/friends", function(req, res) {
-        //used to display a json of all possible friends
-      });
-    
-    //other routes..
-}
+var friends =
+[
+    {
+        "name":"Ahmed",
+        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores":[
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1]
+    }
+]; 
+
+app.get("/api/friends", function(req, res) {
+    //used to display a json of all possible friends
+    return res.json(friends);    
+});
+app.post("/api/friends",function(req,res){
+
+});
+//other routes..
+
   
--- app/routing/htmlroutes.js
+++ app/routing/htmlroutes.js
@@ -2,9 +2,11 @@ var express = require('express');
 var path = require("path");
 var app = module.exports = express();  
 
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/home.html"));
+  });
+  
 
 app.get("/survey", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/survey.html"));
     });
-
-

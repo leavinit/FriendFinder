2635643fe922e3e0d55ba6d32f6d8017e0d40992
Adam S <leavinit@gmail.com>
2019-05-30 19:18:28 -0500

    Ironed out bugs in sending the survey json.

--- app/routing/apiroutes.js
+++ app/routing/apiroutes.js
@@ -1,34 +1,48 @@
 var express = require('express');
 var path = require("path");
 var app = module.exports = express();  
+var allFriends = require("../data/friends")
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 
-var friends =
-[
-    {
-        "name":"Ahmed",
-        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-        "scores":[
-            5,
-            1,
-            4,
-            4,
-            5,
-            1,
-            2,
-            5,
-            4,
-            1]
-    }
-]; 
+// var allFriends =
+// [
+//     {
+//         "name":"Ahmed",
+//         "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+//         "scores":[
+//             5,
+//             1,
+//             4,
+//             4,
+//             5,
+//             1,
+//             2,
+//             5,
+//             4,
+//             1]
+//     }
+// ]; 
 
 app.get("/api/friends", function(req, res) {
     //used to display a json of all possible friends
-    return res.json(friends);    
+    return res.json(allFriends);    
 });
 app.post("/api/friends",function(req,res){
+    //route to add a new friend to the allFriends object when the user sends 
+    console.log(req.body);
+    var newFriend = req.body;
+    console.log("adding via /api/friends(?)")
+    console.log(newFriend);
+    res.send(req.body);
+})
 
-});
-//other routes..
+// app.post('/api/friends', function(req, res){
+//     console.log("req.body: " + req.body);
+//  res.send(req.body);
+// });
+
 
   
--- server.js
+++ server.js
@@ -5,12 +5,13 @@ var app = express();
 var PORT = 8080;
 
 
-var htmlRoutes = require("./app/routing/htmlroutes")
+var htmlRoutes = require("./app/routing/htmlroutes");
 var apiRoutes = require("./app/routing/apiroutes");
 
 app.use(htmlRoutes);
 app.use(apiRoutes);
 app.use(express.urlencoded({ extended: true }));
+
 app.use(express.json());
 
 app.listen(3000);
